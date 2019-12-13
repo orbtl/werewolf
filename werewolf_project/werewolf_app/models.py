a87fb9c0cc22c4037e608002c1da32720246802a
orbtl <twinsendude@gmail.com>
2019-12-13 09:19:29 -0800

    added successes attribute to Role model class

--- werewolf_project/werewolf_app/models.py
+++ werewolf_project/werewolf_app/models.py
@@ -34,7 +34,7 @@ class Game(models.Model):
     # Game Relationships:
     host = models.ForeignKey(User, related_name="games_hosted", on_delete=models.SET_NULL, null=True)
     players = models.ManyToManyField(User, related_name="games_joined") # I believe we need this in addition to roles for each player so we can have the players join the lobby before they are assigned roles.  I suppose an alternative owuld be to assign them a role initially by default that would be called something like "noRole" or "lobbyRole" etc
-    
+    # roles = each role associated with this game
     
 
 
@@ -47,6 +47,7 @@ class Role(models.Model):
     role_notes = models.CharField(max_length=255, null=True) # Not sure we'll need this, but I want it here in case we need to put special notes on a role a player played to explain that they became a werewolf from being turned by the accursed one, etc
     primary_ammo = models.IntegerField(default=0) # Used to keep track of whether special abilities have been used, such as the gypsy questions, or witch potion
     secondary_ammo = models.IntegerField(default=0) # Used to keep track of secondary special abilities, like witch's poison
+    successes = models.IntegerField(default=0) # Used to keep track of things like how many times a seer correctly found a werewolf, etc
     isActivePlayer = models.BooleanField(default=True) # to deal with players being kicked?
     role_name = models.CharField(max_length=45, null=True) #Name of Role, choose from:
     secondary_role_name = models.CharField(max_length=45, null=True) # Need this for roles like Lover that can be used in conjunction with other roles like werewolf
@@ -65,7 +66,6 @@ class Role(models.Model):
 
 
     ### this was the old setup of relationships
-    # roles = each role associated with this game
     # werewolves = models.ManyToManyField(User, related_name="games_werewolf")
     # villagers = models.ManyToManyField(User, related_name="games_villager")
     # village_idiot = models.ForeignKey(User, related_name="games_village_idiot", on_delete=models.SET_NULL, null=True)
