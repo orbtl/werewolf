fcd0b9c1eefd5764c226193749cd30d71d89e6d3
orbtl <twinsendude@gmail.com>
2019-12-13 10:57:16 -0800

    completed most basic first draft of /home layout

--- werewolf_project/werewolf_app/views.py
+++ werewolf_project/werewolf_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import User, Game, Role
 
 def homeIndex(request): # Main Home Page
-    return render(request, 'homeIndex.html')
+    if 'userID' not in request.session or request.session['userID'] == None:
+        messages.error(request, "You must log in to view that page")
+        return redirect('/')
+    context = {
+        'user': User.objects.get(id=request.session['userID']),
+    }
+    return render(request, 'homeIndex.html', context)
 
 def header(request): #partial render header
     context = {
         'user': User.objects.get(id=request.session['userID']),
     }
     return render(request, 'partial/header.html', context)
+
