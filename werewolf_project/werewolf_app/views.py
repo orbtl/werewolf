ade28153fd76e6a343ce996531e66d646b87ae56
orbtl <twinsendude@gmail.com>
2019-12-13 10:28:38 -0800

    got a dynamically loaded header working with basic functionality and an options dropdown

--- werewolf_project/werewolf_app/views.py
+++ werewolf_project/werewolf_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
+from .models import User, Game, Role
 
 def homeIndex(request): # Main Home Page
     return render(request, 'homeIndex.html')
 
 def header(request): #partial render header
-    return render(request, 'partial/header.html')
+    context = {
+        'user': User.objects.get(id=request.session['userID']),
+    }
+    return render(request, 'partial/header.html', context)
