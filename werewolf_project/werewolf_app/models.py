efc637f67db16be1e9c4107fe0e40be920abae96
orbtl <twinsendude@gmail.com>
2019-12-12 17:08:05 -0800

    fleshed out roles to include important attributes for role class, such as ammo to keep track of special abilities availability, etc

--- werewolf_project/werewolf_app/models.py
+++ werewolf_project/werewolf_app/models.py
@@ -5,6 +5,7 @@ class Game(models.Model):
     # Game Options:
     max_players = models.IntegerField()
     num_werewolves = models.IntegerField()
+    has_
     # Game Properties:
     started = models.BooleanField(default=False) # whether the game has started or is still in lobby -- do we need this or can we just use turn number or phase?
     ended = models.BooleanField(default=False) # whether the game is over
@@ -14,7 +15,7 @@ class Game(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     # Game Relationships:
     host = models.ForeignKey(User, related_name="games_hosted", on_delete=models.SET_NULL, null=True)
-    players = models.ManyToManyField(User, related_name="games_joined")
+    players = models.ManyToManyField(User, related_name="games_joined") # I believe we need this in addition to roles for each player so we can have the players join the lobby before they are assigned roles.  I suppose an alternative owuld be to assign them a role initially by default that would be called something like "noRole" or "lobbyRole" etc
     # roles = each role associated with this game
     
 
@@ -23,8 +24,13 @@ class Role(models.Model):
     player = models.ForeignKey(User, related_name="roles", on_delete=models.SET_NULL, null=True)
     game = models.ForeignKey(Game, related_name="roles", on_delete=models.SET_NULL, null=True)
     isAlive = models.BooleanField(default=True)
+    turn_died = models.IntegerField(null=True) # keep track of what turn the player died
+    role_notes = models.CharField(max_length=255, null=True) # Not sure we'll need this, but I want it here in case we need to put special notes on a role a player played to explain that they became a werewolf from being turned by the accursed one, etc
+    primary_ammo = models.IntegerField(default=0) # Used to keep track of whether special abilities have been used, such as the gypsy questions, or witch potion
+    secondary_ammo = models.IntegerField(default=0) # Used to keep track of secondary special abilities, like witch's poison
     isActivePlayer = models.BooleanField(default=True) # to deal with players being kicked?
-    role_name = models.CharField(max_length=45) #Name of Role, choose from:
+    role_name = models.CharField(max_length=45, null=True) #Name of Role, choose from:
+    secondary_role_name = models.CharField(max_length=45, null=True) # Need this for roles like Lover that can be used in conjunction with other roles like werewolf
     #role_name options: cupid,lover,werewolf,villager,village_idiot,twin,accursed_one,seer,witch,defender,hunter,wild_child,role_model,little_child,rusty_knight,elder,angel,gypsy
     
 
