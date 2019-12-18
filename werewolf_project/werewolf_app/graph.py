6a5eb1e97effa220b287b04a7d32474e021991d1
orbtl <twinsendude@gmail.com>
2019-12-18 14:43:16 -0800

    updated profile page graph setup/layout

--- werewolf_project/werewolf_app/graph.py
+++ werewolf_project/werewolf_app/graph.py
@@ -1,14 +1,29 @@
 from plotly.offline import plot
 import plotly.graph_objs as go
 
-def makeGraph(x_data, y_data):
+def makeGraph(x_data, y_data): # profile page graph
     fig = go.Figure()
     scatter = go.Scatter(x=x_data, y=y_data, mode='lines', name='test', opacity=1, marker_color='green')
     fig.add_trace(scatter)
+    fig.update_layout(
+        xaxis=dict(
+            showgrid=False,
+            showticklabels=False,
+        ),
+        yaxis=dict(
+            showgrid=False,
+        ), showlegend=False, margin=dict(
+            l=0,
+            t=0,
+            b=0,
+            r=0,
+            pad=0,
+        ), width=500, height=295,
+    )
     plt_div = plot(fig, output_type='div')
     return plt_div
    
-def doubleGraph(x_data, y_dataW, y_dataV):
+def doubleGraph(x_data, y_dataW, y_dataV): # end of game graph
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x_data, y=y_dataW, name="Werewolves Alive", line_color="red"))
     fig.add_trace(go.Scatter(x=x_data, y=y_dataV, name="Villagers Alive", line_color="blue"))
@@ -16,7 +31,7 @@ def doubleGraph(x_data, y_dataW, y_dataV):
     plt_div = plot(fig, output_type="div")
     return plt_div
 
-def indexGraph(x_data, y_dataW, y_dataV):
+def indexGraph(x_data, y_dataW, y_dataV): # main index graph
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x_data, y=y_dataW, name="Werewolf Winrate", line_color="red"))
     fig.add_trace(go.Scatter(x=x_data, y=y_dataV, name="Villager Winrate", line_color="blue"))
