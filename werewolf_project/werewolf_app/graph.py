3ce2f8316aa9db471c48f2f71d16b19045fc041c
orbtl <twinsendude@gmail.com>
2019-12-18 14:57:45 -0800

    updated graph formatting some more

--- werewolf_project/werewolf_app/graph.py
+++ werewolf_project/werewolf_app/graph.py
@@ -7,18 +7,20 @@ def makeGraph(x_data, y_data): # profile page graph
     fig.add_trace(scatter)
     fig.update_layout(
         xaxis=dict(
-            showgrid=False,
+            showgrid=True,
             showticklabels=False,
+            gridcolor='lightgray',
         ),
         yaxis=dict(
-            showgrid=False,
+            showgrid=True,
+            gridcolor='lightgray',
         ), showlegend=False, margin=dict(
             l=0,
             t=0,
             b=0,
             r=0,
             pad=0,
-        ), width=500, height=295,
+        ), width=500, height=295, plot_bgcolor='white', paper_bgcolor='white',
     )
     plt_div = plot(fig, output_type='div')
     return plt_div
@@ -37,18 +39,20 @@ def indexGraph(x_data, y_dataW, y_dataV): # main index graph
     fig.add_trace(go.Scatter(x=x_data, y=y_dataV, name="Villager Winrate", line_color="blue"))
     fig.update_layout(
         xaxis=dict(
-            showgrid=False,
+            showgrid=True,
             showticklabels=False,
+            gridcolor='lightgray',
         ),
         yaxis=dict(
-            showgrid=False,
+            showgrid=True,
+            gridcolor='lightgray',
         ), showlegend=False, margin=dict(
             l=0,
             t=0,
             b=0,
             r=0,
             pad=0,
-        ), width=358, height=295,
+        ), width=358, height=295, plot_bgcolor='white', paper_bgcolor='white',
     )
     plt_div = plot(fig, output_type="div")
     return plt_div
